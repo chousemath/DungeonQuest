8e6ffe92bfc303526e34e23dca6b20f798e7218b
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-25 03:14:06 +0900

    added selection of difficult level to the game

--- DungeonQuest/main.cpp
+++ DungeonQuest/main.cpp
@@ -12,6 +12,10 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     string playerName;
     int playerAge;
+    
+    // all symbols discarded at compile time
+    enum gameDifficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
+    
     cout << "Welcome weary traveller, you have come a long, long way..." << endl;
     cout << "Tell me traveller, what is your name? ";
     cin >> playerName;
@@ -22,6 +26,35 @@ int main(int argc, const char * argv[]) {
         cout << "The perils ahead of your are too great at this time, be gone!" << endl;
     } else {
         cout << "Ah, you have come to the right place at the right time..." << endl;
+        cout << "How difficult of a quest are you prepared for traveller?" << endl;
+        cout << "\t0 - NOVICE" << endl;
+        cout << "\t1 - EASY" << endl;
+        cout << "\t2 - NORMAL" << endl;
+        cout << "\t3 - HARD" << endl;
+        cout << "\t4 - UNBEATABLE" << endl;
+        cout << "\nENTER DIFFICULTY NUMBER: ";
+        int myDifficulty;
+        cin >> myDifficulty;
+        switch (myDifficulty) {
+            case NOVICE:
+                cout << "You have chosen the NOVICE level adventure~" << endl;
+                break;
+            case EASY:
+                cout << "You have chosen the EASY level adventure~" << endl;
+                break;
+            case NORMAL:
+                cout << "You have chosen the NORMAL level adventure~" << endl;
+                break;
+            case HARD:
+                cout << "You have chosen the HARD level adventure~" << endl;
+                break;
+            case UNBEATABLE:
+                cout << "You have chosen the UNBEATABLE level adventure~" << endl;
+                break;
+            default:
+                cout << "You have chosen an undefined difficulty level" << endl;
+                break;
+        }
     }
     cout << "GAME OVER" << endl;
     return 0;
