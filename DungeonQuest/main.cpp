aa62b845cdb8b8d38860253ce96c1005e6f31787
Joseph Sungpil Choi <chousemath@gmail.com>
2017-06-03 12:45:26 +0900

    added string reversal

--- DungeonQuest/main.cpp
+++ DungeonQuest/main.cpp
@@ -6,7 +6,12 @@
 //  Copyright © 2017 Joseph Sungpil Choi. All rights reserved.
 //
 
+// the '#' symbol is a directive to the C-preprocessor
+// cuts-and-pastes a library into your source code
 #include <iostream>
+#include <string>
+#include <locale>
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -20,6 +25,11 @@ int main(int argc, const char * argv[]) {
     cout << "Tell me traveller, what is your name? ";
     cin >> playerName;
     cout << "It is not by chance that we meet here tonight " + playerName << "." << endl;
+    std::string copy(playerName); // make a copy of the string
+    std::reverse(copy.begin(), copy.end());
+    cout << "Your name backwards, by the way, is: "
+         << copy
+         << endl;
     cout << "Your garments hide your features, what, may I ask, is your age? ";
     cin >> playerAge;
     if (playerAge < 15){
@@ -38,21 +48,27 @@ int main(int argc, const char * argv[]) {
         switch (myDifficulty) {
             case NOVICE:
                 cout << "You have chosen the NOVICE level adventure~" << endl;
+                cout << "You're kidding me right?" << endl;
                 break;
             case EASY:
                 cout << "You have chosen the EASY level adventure~" << endl;
+                cout << "Psshhh get out of my site you simpleton!" << endl;
                 break;
             case NORMAL:
                 cout << "You have chosen the NORMAL level adventure~" << endl;
+                cout << "A normal adventure for a normal person. You dissapoint me..." << endl;
                 break;
             case HARD:
                 cout << "You have chosen the HARD level adventure~" << endl;
+                cout << "Good things come to those who seek a challenge." << endl;
                 break;
             case UNBEATABLE:
                 cout << "You have chosen the UNBEATABLE level adventure~" << endl;
+                cout << "A challenge you seek? A challenge you will have!" << endl;
                 break;
             default:
                 cout << "You have chosen an undefined difficulty level" << endl;
+                cout << "You must be totally crazy traveller, I like it!" << endl;
                 break;
         }
     }
